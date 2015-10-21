41d8d333a6ae95d599de8b214a0c1b47ae9b913b
At15 <at15@outlook.com>
2015-10-21 12:32:47 +0800

    Update run-check.sh
    Fix outdated comment

--- run-check.sh
+++ run-check.sh
@@ -2,9 +2,8 @@
 
 # a shortcut for running all test commands
 
-# TODO:this require phpunit install globaly, should use the vendor version instead
+# use the vendor version instead
 ./vendor/bin/phpunit
 
-# phpcs
-
+# use the vendored phpcs
 ./vendor/bin/phpcs --standard=PSR2 ./src
